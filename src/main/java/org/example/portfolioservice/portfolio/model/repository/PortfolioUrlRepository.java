31bf432a3b5f72cb81ebf81af18e9d9e6badc47e
dochmai382 <dochmai382@gmail.com>
2025-05-21 22:16:37 +0900

    refactor: 중복 어노테이션 제거

--- src/main/java/org/example/portfolioservice/portfolio/model/repository/PortfolioUrlRepository.java
+++ src/main/java/org/example/portfolioservice/portfolio/model/repository/PortfolioUrlRepository.java
@@ -2,11 +2,10 @@ package org.example.portfolioservice.portfolio.model.repository;
 
 import org.example.portfolioservice.portfolio.model.entity.PortfolioUrl;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
+
 public interface PortfolioUrlRepository extends JpaRepository<PortfolioUrl, Long> {
     List<PortfolioUrl> findAllByPortfolioUrlIdIn(List<Long> ids);
 }
